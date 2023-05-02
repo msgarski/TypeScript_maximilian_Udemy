d96ce8f6c94f06119ff08ebab8eac115516984be
msgarski <msgarski@gmail.com>
2023-05-02 19:52:39 +0200

    starting_real_typescript_project

---
+++ adv-types-01-starting-setup/src/app.ts
@@ -0,0 +1,17 @@
+// Code goes here!
+// type Admin = {
+//   name: string;
+//   privileges: string[];
+// };
+
+// type Employee = {
+//   name: string;
+//   startDate: Date;
+// };
+
+// type ElevatedEmployee = Admin & Employee;
+
+// console.log("nic do dodania.?..");
+const numer: number = 22;
+let tablica: string[];
+console.log("mój projekt");
